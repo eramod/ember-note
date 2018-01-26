1ba7be46b989b0515174ceef12121abc3205fa6a
Dianne Eramo <dianne@allovue.com>
2018-01-26 17:13:35 -0500

    Chapter 1: Starting Your App With Ember
    Set up a new Ember application with a register route that allows you to register a user.
    
    Added Ember CLI Mirage with a user model to set up a fake client side server for the app

---
+++ app/routes/register.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+
+  actions: {
+    addNew() {
+      var user = this.store.createRecord('user', {
+        name: this.controller.get('name')
+      });
+      user.save().then(() => {
+        let name = this.controller.get('name');
+        this.controller.set('message', 'A new user with the name ' + name + ' was added!');
+        this.controller.set('name', null);
+      },
+      function(){
+        alert('save failed');
+      });
+    }
+  }
+});
