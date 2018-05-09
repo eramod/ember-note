50041dabbecefa4e40fff53e3a9ad12ba28b2889
Dianne Eramo <dianne@allovue.com>
2018-05-09 14:34:10 -0400

    Ch 5: Modeling Your Data
    * Add the ability to delete a note
    * Learn about the Ember Data store and how it’s used to interact with a RESTful API
    * Learn about how a model is a representation of a record in persistent storage and a record is an instance of a model that actually holds data

--- app/routes/notebooks/notes/note.js
+++ app/routes/notebooks/notes/note.js
@@ -7,6 +7,14 @@ export default Ember.Route.extend({
   actions: {
     close() {
       this.transitionTo('notebooks.notes');
+    },
+    deleteNote(note) {
+      note.destroyRecord();
+      // destroyRecord is equivalent to calling deleteRecord() followed by save(),
+      //  so we can write out the above in long form.
+      //  note.deleteRecord(); // Removes record from the Ember store.
+      //   If you just do this, the record will return when you reload the UI
+      //  note.save(); // Saves that deletion to persistent storage
     }
   }
 });
