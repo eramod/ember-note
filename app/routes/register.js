61c9460101cf2e43ba19b68d85c51610699bdacc
Dianne Eramo <dianne@allovue.com>
2018-06-18 16:03:16 -0400

    Ch 7: Reusing Code in Ember: Mixins
    * Create a mixin to validate email when a user registers as well as the length of a note title
    * Unlike utilities which can only contain functions, mixins can contain both properties and functions, which means they can be used to abstract state as well as behavior

--- app/routes/register.js
+++ app/routes/register.js
@@ -1,20 +1,25 @@
 import Ember from 'ember';
+import ValidationFunctions from 'ember-note/mixins/validation-functions';
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(ValidationFunctions,{
 
   actions: {
     addNew() {
-      var user = this.store.createRecord('user', {
-        name: this.controller.get('name')
-      });
-      user.save().then(() => {
-        let name = this.controller.get('name');
-        this.controller.set('message', 'A new user with the name ' + name + ' was added!');
-        this.controller.set('name', null);
-      },
-      function(){
-        alert('save failed');
-      });
+      let name = this.controller.get('name');
+      if ( this.isValidEmail(name) ) {
+        let user = this.store.createRecord('user', {
+          name: name
+        });
+        user.save().then(() => {
+          this.controller.set('message', 'A new user with the name ' + name + ' was added!');
+          this.controller.set('name', null);
+        },
+        function(){
+          alert('save failed');
+        });
+      } else {
+        alert('Invalid email address.');
+      }
     }
   }
 });
